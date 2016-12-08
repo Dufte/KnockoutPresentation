55be83cc6dec9f576084c608016d38ddae5a1a72
Dufte <std8338@gmx.de>
2016-12-08 14:14:51 +0100

    Initial Commit

---
+++ TestWithRavenDb/TestWithRavenDb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWithRavenDb.Startup))]
+namespace TestWithRavenDb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
